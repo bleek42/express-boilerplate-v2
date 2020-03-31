4663dfeec78cd54bbf6b04ae9c42009e0a2c1dc7
bleek42 <brandonleek42@gmail.com>
2020-03-31 10:40:40 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const uuid = require('uuid/v4');
+
+const app = express();
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!');
+});
+
+const errorHandler = (error, req, res, next) => {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'Server error' } };
+  } else {
+    response = { message: error.message, error };
+  }
+
+  res.status(500).json(response);
+};
+
+app.use(errorHandler);
+
+module.exports = app;
